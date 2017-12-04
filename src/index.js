bb0d3d64daa1ac2ccfde1f415d925e30d883f58e
xoqem <sinfree@gmail.com>
2017-12-04 15:44:00 -0800

    Add react

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const element = document.createElement('div');
-element.innerHTML = 'React Redux Base';
 document.body.appendChild(element);
+
+ReactDOM.render(
+  <h1>React Redux Base</h1>,
+  element
+);
--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,19 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react']
+          }
+        }
+      }
+    ]
   }
 };
