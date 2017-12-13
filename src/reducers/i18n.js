7c9af64a51640aae30aa51d6e5f246c24d0885a1
xoqem <sinfree@gmail.com>
2017-12-13 14:58:45 -0800

    Add spanish

--- src/reducers/i18n.js
+++ src/reducers/i18n.js
@@ -1,11 +1,13 @@
 import en from 'i18n/en';
+import es from 'i18n/es';
 
 const defaultState = {
   locale: 'en',
   messages: {
-    en
+    en,
+    es
   },
-  locales: ['en']
+  locales: ['en', 'es']
 };
 
 export default (state = defaultState, action) => {
